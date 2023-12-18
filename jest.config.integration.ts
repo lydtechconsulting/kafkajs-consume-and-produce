6a9113e31886bc96c7f11a566d5c556da3bd677a
rgolder1 <rgolder1@yahoo.com>
2023-12-18 09:10:55 +0000

    Updates

--- jest.config.integration.ts
+++ jest.config.integration.ts
@@ -1,9 +1,4 @@
 module.exports = {
-	// globals: {
-	// 	'ts-jest': {
-	// 		tsconfig: 'tsconfig.json'
-	// 	}
-	// },
 	moduleNameMapper: {
 		'^jose/(.*)$': '<rootDir>/node_modules/jose/dist/node/cjs/$1'
 	},
@@ -12,11 +7,9 @@ module.exports = {
 	moduleFileExtensions: ['ts', 'js'],
 	resetMocks: true,
 	setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-	// testResultsProcessor: 'jest-sonar-reporter',
 	transform: {
 		'^.+\\.(ts|tsx)$': 'ts-jest'
 	},
 	globalSetup: '<rootDir>/jest.setup.integration.ts',
-	globalTeardown: '<rootDir>/jest.teardown.integration.ts',
-	testEnvironment: '<rootDir>/jest.env.integration.ts'
+	globalTeardown: '<rootDir>/jest.teardown.integration.ts'
 };
